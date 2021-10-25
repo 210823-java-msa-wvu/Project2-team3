6d641e1f193390062851b04d3a97171a43d0f10a
AnthonyVu0504 <89420503+AnthonyVu0504@users.noreply.github.com>
2021-10-25 14:01:11 -0500

    Adding new code to dice component and html

--- front-end/p2/src/app/components/dice/dice.component.ts
+++ front-end/p2/src/app/components/dice/dice.component.ts
@@ -14,7 +14,10 @@ export class DiceComponent implements OnInit {
   }
   public diceValues: Array<number> = [0, 0 ,0 ,0 ,0 ,0];
   public diceNumber: number = 0;
+  public diceNumberOne: number = 0;
+  public diceNumberTwo: number = 0;
   public natural: number = 20;
+  public result: string = '';
 
   diceRoll() {
     for (let i = 0; i < this.diceValues.length; i++) {
@@ -25,5 +28,39 @@ export class DiceComponent implements OnInit {
   diceroll(){
     this.diceNumber = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
   }
+
+  dicerollOne(){
+    this.diceNumberOne = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+  }
+
+  dicerollTwo(){
+    this.diceNumberTwo = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
+  }
+
+  clearRoll() {
+    this.diceNumber = 0;
+  }
+  
+  clearDiceOne(){
+    this.diceNumberOne = 0;
+  }
+
+  clearDiceTwo() {
+    this.diceNumberTwo = 0;
+  }
+
+  rollResults() {
+    if (this.diceNumber > this.diceNumberTwo){
+      this.result = 'Attacking Player has Landed a Hit on the Target!';
+    } else if (this.diceNumberTwo > this.diceNumber){
+      this.result = 'The Defending Player has Successfully Evaded the Attack!';
+    } else if (this.diceNumber == this.diceNumberTwo){
+      this.result = 'The Attacker and Defender are at a Stand Still!';
+    } else {
+      this.result = '';
+    }
+  }
+
+
 
 }
